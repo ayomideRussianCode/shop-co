1713f462230e024776bd2965d60a0d6b21b507f3
ayomideRussianCode <ayindeayomide7@gmail.com>
2025-05-23 04:20:32 +0100

    navbar responsiveness set

--- src/components/MobileMenu.jsx
+++ src/components/MobileMenu.jsx
@@ -27,26 +27,3 @@ export default MobileMenu;
 
 
 
-// import { motion } from "framer-motion";
-
-
-// const MobileMenu = ({ isOpen, onClose }) => {
-//   return (
-//     <motion.div
-//       initial={{ x: "100%" }}
-//       animate={{ x: isOpen ? "0%" : "100%" }}
-//       transition={{ duration: 0.3 }}
-//       className="fixed top-0 right-0 h-full w-64 bg-white shadow-lg z-50 p-6"
-//     >
-//       <button className="text-2xl mb-6" onClick={onClose}>✖</button>
-//       <nav className="flex flex-col space-y-4">
-//         <a href="#" className="text-lg text-gray-800 hover:text-black">Shop</a>
-//         <a href="#" className="text-lg text-gray-800 hover:text-black">On Sale</a>
-//         <a href="#" className="text-lg text-gray-800 hover:text-black">New Arrivals</a>
-//         <a href="#" className="text-lg text-gray-800 hover:text-black">Brands</a>
-//       </nav>
-//     </motion.div>
-//   );
-// };
-
-// export default MobileMenu;
