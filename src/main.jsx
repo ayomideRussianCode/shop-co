1dd6aedd1f84557d4cbfb85da61446d8d7da845c
ayomideRussianCode <ayindeayomide7@gmail.com>
2025-05-26 12:29:19 +0100

    productdetailpage linked with product card and routing is now possible

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import HomePage from './pages/HomePage.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./index.css";
+import HomePage from "./pages/HomePage.jsx";
+import ProductDetailPage from "./pages/ProductDetailPage.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <HomePage/>
-  </StrictMode>,
-)
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/pages/ProductDetailPage.jsx/:id" element={<ProductDetailPage />} />
+      </Routes>
+    </Router>
+  </StrictMode>
+);
---
+++ src/pages/ProductDetailPage.jsx
@@ -0,0 +1,12 @@
+import Navbar from '../components/NavBar';
+import TopBar from '../components/TopBar';
+
+function ProductDetailPage() {
+  return (
+    <>
+    <TopBar/>
+    <Navbar/>
+    </>
+  )
+}
+export default ProductDetailPage
